0ef085b98c30f35080343fc4de2192e659a8e48a
Kevin Gomez <gomezdiezkevinmateo@gmail.com>
2024-09-12 10:52:48 -0500

    class reminder and class event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -5,14 +5,29 @@ from typing import ClassVar
 from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
     reminder_not_found_error, slot_not_available_error
 
+@dataclass
+class Reminder:
 
-# TODO: Implement Reminder class here
+    EMAIL:str = field(init=False, default="email")
+    SYSTEM: str = field(init=False, default= "system" )
 
+    date_time = datetime
+    type: str = EMAIL
 
-# TODO: Implement Event class here
+    def __str__(self):
+        return f"Reminder on {self.date_time} of type {type}"
 
+@dataclass
+class Event:
 
-# TODO: Implement Day class here
+    title = str
+    description = str
+    date_ = date
+    start_at = time
+    end_at = time
 
+    reminders: list[Reminder] = field(default_factory = list)
 
-# TODO: Implement Calendar class here
+    id: str = field(default_factory = generate_unique_id)
+
+    #def add_reminder(self):
